649ffb930c54533ea041fc0ffd515d67bd6cc0f3
Jamie Catania <catania.jamie@gmail.com>
2017-12-13 23:31:47 -0500

    Playing with fading buttons on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+jQuery(document).ready(function ($) {
 
     /* ======= Scrollspy ======= */
     $('body').scrollspy({ target: '#page-nav-wrapper', offset: 100 });
@@ -15,12 +15,33 @@ $(document).ready(function () {
 
     });
 
-    /* ======= Fixed page nav when scrolled ======= */
-    $(window).on('scroll resize load', function () {
 
+
+
+
+
+
+
+    /* ======= Fixed page nav when scrolled ======= */
+    var didScroll;
+    // on scroll, let the interval function know the user has scrolled
+    $(window).on('scroll resize load', function (event) {
+        didScroll = true;
+    });
+    // run hasScrolled() and reset didScroll status
+    setInterval(function () {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+    function hasScrolled() {
         $('#page-nav-wrapper').removeClass('fixed');
 
-        var scrollTop = $(this).scrollTop();
+        var scrollTop = $(this).scrollTop(),
+            windowHeight = $(this).height();
+
+        // Handle whether to fix page nav to top
         var topDistance = $('#page-nav-wrapper').offset().top;
 
         if ((topDistance) > scrollTop) {
@@ -32,7 +53,23 @@ $(document).ready(function () {
             $('body').addClass('sticky-page-nav');
         }
 
-    });
+        // Handle whether to fade intro-btn
+        var introBtnDistance = $('#intro-btn').offset().top;
+
+        console.log(`scrollTop: ${scrollTop} | windowHeight: ${windowHeight}`)
+
+        if (scrollTop >= windowHeight * .66) {
+            console.log('Fading In...');
+            $('#intro-btn').hide().fadeIn();
+        }
+        else {
+            console.log('Fading Out...');
+            $('#intro-btn').show().fadeOut();
+
+        }
+    }
+
+
 
     /* ======= Chart ========= */
 
@@ -91,4 +128,68 @@ $(document).ready(function () {
         return false;
     });
 
+    // animated underline on navbar
+    var $el,
+        leftPos,
+        newWidth,
+        $mainNav = $(".page-nav");
+
+    $mainNav.append("<li id='magic-line'></li>");
+    var $magicLine = $("#magic-line");
+
+    $magicLine
+        .width($(".active").width())
+        .css("left", $(".active a").position().left)
+        .data("origLeft", $magicLine.position().left)
+        .data("origWidth", $magicLine.width());
+
+    // move magic-line on hover
+    $(".page-nav li a").hover(
+        function () {
+            $el = $(this);
+            leftPos = $el.position().left;
+            newWidth = $el.parent().width();
+            $magicLine.stop().animate({
+                left: leftPos,
+                width: newWidth
+            });
+        },
+        function () {
+            $magicLine.stop().animate({
+                left: $magicLine.data("origLeft"),
+                width: $magicLine.data("origWidth")
+            });
+        }
+    );
+
+    // lock magic line to new position on click
+    $(".page-nav li a").click(
+        function () {
+            $el = $(this);
+            leftPos = $el.position().left;
+            newWidth = $el.parent().width();
+
+            $(".active").removeClass("active");
+            $el.parent().addClass("active");
+
+            $magicLine
+                .width($(".active").width())
+                .css("left", $(".active a").position().left)
+                .data("origLeft", $magicLine.position().left)
+                .data("origWidth", $magicLine.width());
+        });
+
+    // update magic line to new position via scrollspy
+    $('#myScrollspy').on('activate.bs.scrollspy', function () {
+        // do something…
+    })
+
+    // fade magic line out if overview is visible
+    $(window).on("load scroll resize", function () {
+        if ($(".page-nav-wrapper").hasClass("fixed")) {
+            $("#magic-line").fadeIn();
+        } else {
+            $("#magic-line").fadeOut();
+        }
+    });
 });
